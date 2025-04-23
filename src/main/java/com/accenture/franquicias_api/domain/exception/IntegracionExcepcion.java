a399611dfe474cd67f80b53856147822c797d054
jhonederMC <jhoneder@hotmail.com>
2025-04-22 20:34:45 -0500

    Clase IntegracionExcepcion
    Se manejan los errores personalizados asociados al funcionamiento del sistema. Esto es para identificar claramente los tipo de errores ocurridos.

---
+++ src/main/java/com/accenture/franquicias_api/domain/exception/IntegracionExcepcion.java
@@ -0,0 +1,39 @@
+package com.accenture.franquicias_api.domain.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class IntegracionExcepcion extends RuntimeException {
+
+    @Getter
+    @AllArgsConstructor
+    public enum Type {
+        ERROR_DB_TRANSACTION("Error en la transacción con la base de datos"),
+        ERROR_EN_PETICION("Error en la petición al servicio"),
+        NO_SE_ENCONTRARON_RESULTADOS("No se encontraron resultados");
+
+
+        private final String errorTecnico;
+
+        public IntegracionExcepcion build(String errorTecnico) {
+            return new IntegracionExcepcion(this, this.getErrorTecnico() + ": " + errorTecnico);
+        }
+
+        public IntegracionExcepcion build() {
+            return new IntegracionExcepcion(this);
+        }
+
+    }
+
+    private final Type type;
+
+    private IntegracionExcepcion(Type type, String message) {
+        super(message);
+        this.type = type;
+    }
+    public IntegracionExcepcion(Type type) {
+        super(type.errorTecnico);
+        this.type = null;
+    }
+}
